1bc14e759ccad010a5f028d239eb4a446b07e9fc
jeremymiller <jeremym@alleninstitute.org>
2018-08-30 14:57:30 -0700

    create calculatePatchSeqQCMetrics2

--- R/update.r
+++ R/update.r
@@ -1,6 +1,5 @@
 #' Update the patchseqtools library
 #' 
 update_patchseqtools <- function() {
-  devtools::install_github("AllenInstitute/patchseqtools", 
-    auth_token = "802976690281f1483c40de46d0a07e9d01a3de08")
+  devtools::install_github("AllenInstitute/patchseqtools", auth_token = "802976690281f1483c40de46d0a07e9d01a3de08")
 }
