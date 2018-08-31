e36bc900ca33022169a49b9bcbb3aa1d2dfa0d58
jeremymiller <jeremym@alleninstitute.org>
2018-08-30 17:14:32 -0700

    new function: compareClusterCalls

--- R/CCA_wrappers.r
+++ R/CCA_wrappers.r
@@ -1,1 +1,79 @@
+#' Reorder a matrix
+#'
+#' This function reorders a matrix by rows of columns
+#'
+#' @param matrix1 a matrix (rows=genes x columns=samples) of gene expression data (e.g., scRNA-seq)
+#' @param by.rows By rows (TRUE; default) or by columns
+#'
+#' @return a reordered matrix
+#'
+reorder_matrix <- function(matrix1, by.rows = TRUE) {
+  if (by.rows == TRUE) {
+    conf.order <- order(apply(matrix1, 1, which.max))
+    matrix1.reordered <- matrix1[conf.order, ]
+  } else {
+    conf.order <- order(apply(matrix1, 2, which.max))
+    matrix1.reordered <- matrix1[, conf.order]
+  }
+  return(matrix1.reordered)
+}
 
+
+#' Compare two cluster sets matched to CCA
+#'
+#' This function takes cluster calls defined in two different data sets and then determines to what
+#' extent these cluster calls match up with cluster calls from CCA.
+#'
+#' @param cl a matrix (rows=genes x columns=samples) of gene expression data (e.g., scRNA-seq)
+#' @param by.rows By rows (TRUE; default) or by columns
+#'
+#' @return a reordered matrix
+#'
+compareClusterCalls <- function(FACs_Patch_cca,
+                              plot.title = NA, plot.silent = TRUE,
+                              heat.colors = colorRampPalette(c("grey99", "orange", "red"))(100),
+                              row.cl.num = min(length(unique(cl)),
+                                               length(unique(ref.cl)))) {
+  library(grid)
+  library(pheatmap)
+
+  # Set up the variables
+  combined.cl <- FACs_Patch_cca@meta.data %>%
+    mutate(ref.cl = paste(protocol, ref.cl))
+  colnames(combined.cl) <- gsub("res.0.8","seurat.cl",colnames(combined.cl))
+
+  cl <- setNames(combined.cl$ref.cl, rownames(FACs_Patch_cca@meta.data))
+  ref.cl <- setNames(combined.cl$seurat.cl, rownames(FACs_Patch_cca@meta.data))
+
+  combined.cl.anno <- combined.cl %>%
+    group_by(ref.cl) %>%
+    summarise(protocol = dplyr::first(protocol),
+              mean.nGene = mean(nGene),
+              #            mean.nUMI = mean(nUMI),
+              n.cells = n()) %>%
+    as.data.frame()
+
+  rownames(combined.cl.anno)  <- combined.cl.anno$ref.cl
+  combined.cl.anno <- combined.cl.anno[,-1]
+
+  # Set up more variables
+  conf1 <- table(cl, ref.cl)
+  conf1 <- sweep(conf1, 1, rowSums(conf1), "/")
+  conf2 <- reorder_matrix(conf1)
+
+  # Cluster co-occurence
+  cl.prop.cocl <- apply(conf1, 2, function(x) {
+    grid1 <- expand.grid(x, x)
+    min.prop <- apply(grid1, 1, min)
+  })
+  cl.prop.cocl.total <- apply(cl.prop.cocl, 1, sum)
+  cl.prop.cocl.m <- matrix(cl.prop.cocl.total, nrow(conf1), nrow(conf1),
+                           dimnames = list(rownames(conf1), rownames(conf1)))
+
+  # Heatmap
+  ph1 <- pheatmap(conf2, cutree_rows = row.cl.num, clustering_method = "ward.D2",
+                  annotation_row = combined.cl.anno,
+                  color = heat.colors, fontsize = 6,
+                  main = plot.title, silent = plot.silent)
+  return(list(conf = conf2, cocl = cl.prop.cocl.m, ph = ph1))
+}
--- R/QC_wrappers.r
+++ R/QC_wrappers.r
@@ -13,7 +13,8 @@
 #' @return a 3 x count matrix of the top confused pairs of clusters with the three columns corresponding
 #'   to mapped cluster, assigned cluster, and fraction of cells incorrectly mapped, respectively.
 #'
-defineClassMarkers <- function(datRef, onClasses, offClasses, numMarkers = 50) {
+defineClassMarkers <- function(datRef, onClasses, 
+  offClasses, numMarkers = 50) {
   # Data prep and errors
   if (is.null(colnames(datRef))) 
     colnames(datRef) = as.character(1:length(colnames(datRef)))
@@ -29,20 +30,25 @@ defineClassMarkers <- function(datRef, onClasses, offClasses, numMarkers = 50) {
   names(onClasses) <- names(offClasses) <- samples
   
   # Caclulate proportionsa and medians
-  propExpr <- do.call("cbind", tapply(names(onClasses), onClasses, 
-    function(x) rowMeans(datRef[, x] > 1)))
+  propExpr <- do.call("cbind", tapply(names(onClasses), 
+    onClasses, function(x) rowMeans(datRef[, x] > 
+      1)))
   propExpr <- propExpr[, levels(onClasses)]
-  medianExpr <- do.call("cbind", tapply(names(onClasses), onClasses, 
-    function(x) rowMeans(datRef[, x])))
-  medianExpr <- log2(medianExpr[, levels(onClasses)] + 1)
+  medianExpr <- do.call("cbind", tapply(names(onClasses), 
+    onClasses, function(x) rowMeans(datRef[, x])))
+  medianExpr <- log2(medianExpr[, levels(onClasses)] + 
+    1)
   rownames(propExpr) <- rownames(medianExpr) <- rownames(datRef)
   
-  propExprC <- do.call("cbind", tapply(names(offClasses), offClasses, 
-    function(x) rowMeans(datRef[, x] > 1)))
+  propExprC <- do.call("cbind", tapply(names(offClasses), 
+    offClasses, function(x) rowMeans(datRef[, 
+      x] > 1)))
   propExprC <- propExprC[, levels(offClasses)]
-  medianExprC <- do.call("cbind", tapply(names(offClasses), offClasses, 
-    function(x) rowMeans(datRef[, x])))
-  medianExprC <- log2(medianExprC[, levels(offClasses)] + 1)
+  medianExprC <- do.call("cbind", tapply(names(offClasses), 
+    offClasses, function(x) rowMeans(datRef[, 
+      x])))
+  medianExprC <- log2(medianExprC[, levels(offClasses)] + 
+    1)
   rownames(propExprC) <- rownames(medianExprC) <- rownames(datRef)
   
   # Define and return markers
@@ -51,10 +57,12 @@ defineClassMarkers <- function(datRef, onClasses, offClasses, numMarkers = 50) {
   for (cn in colnames(propExpr)) {
     a = (propExpr[, cn] - apply(propExpr[, colnames(propExpr) != 
       cn], 1, mean))
-    b = ((medianExpr[, cn] - rowMeans(medianExpr[, colnames(medianExpr) != 
-      cn]))/(medianExpr[, cn] + 1))
-    kp = a * b * (a > 0) * (b > 0) * propExpr[, cn] * medianExpr[, 
-      cn] * (medianExpr[, cn] >= 5) * (propExpr[, cn] >= 0.5)
+    b = ((medianExpr[, cn] - rowMeans(medianExpr[, 
+      colnames(medianExpr) != cn]))/(medianExpr[, 
+      cn] + 1))
+    kp = a * b * (a > 0) * (b > 0) * propExpr[, 
+      cn] * medianExpr[, cn] * (medianExpr[, 
+      cn] >= 5) * (propExpr[, cn] >= 0.5)
     markers[[paste0(cn, "_on")]] <- make.names(names(head(-sort(-kp), 
       numMarkers)))
   }
@@ -62,10 +70,13 @@ defineClassMarkers <- function(datRef, onClasses, offClasses, numMarkers = 50) {
   for (cn in colnames(propExprC)) {
     a = (propExprC[, cn] - apply(propExprC[, colnames(propExprC) != 
       cn], 1, max))
-    b = ((medianExprC[, cn] - apply(medianExprC[, colnames(medianExprC) != 
-      cn], 1, max))/(medianExprC[, cn] + 1))
-    kp = a * b * (a > 0) * (b > 0) * sqrt(medianExprC[, cn])
-    markers[[cn]] <- make.names(names(head(-sort(-kp), numMarkers)))
+    b = ((medianExprC[, cn] - apply(medianExprC[, 
+      colnames(medianExprC) != cn], 1, max))/(medianExprC[, 
+      cn] + 1))
+    kp = a * b * (a > 0) * (b > 0) * sqrt(medianExprC[, 
+      cn])
+    markers[[cn]] <- make.names(names(head(-sort(-kp), 
+      numMarkers)))
   }
   
   return(markers)
@@ -96,19 +107,24 @@ defineClassMarkers <- function(datRef, onClasses, offClasses, numMarkers = 50) {
 #' quality_score: Quality score, defined as the Spearman correlation of marker expression between markers expressed in single cell with mean expression of markers in dissociated cell reference data
 #' This function also outputs normalized expression of each 'off'-cell type (defined in compare_cell_types_inh) and we can use the function plotContamHeatmap to show these (each column is one single cell)
 #'
-calculatePatchSeqQCMetrics2 <- function(pat_df, facs_df, markers) {
+calculatePatchSeqQCMetrics2 <- function(pat_df, facs_df, 
+  markers) {
   
-  # This calculates some comparison matrix between each pair of
-  # types
+  # This calculates some comparison matrix between
+  # each pair of types
   facs_df$contam_type = facs_df$major_type
-  aibs_contam_all_broad = calcContamAllTypes(facs_df, markers)
+  aibs_contam_all_broad = calcContamAllTypes(facs_df, 
+    markers)
   aibs_contam_all_broad$contam_type = factor(facs_df$contam_type)
-  aibs_med_exprs_broad = aibs_contam_all_broad %>% dplyr::group_by(contam_type) %>% 
-    dplyr::summarize_all(median) %>% as.data.frame()
+  aibs_med_exprs_broad = aibs_contam_all_broad %>% 
+    dplyr::group_by(contam_type) %>% dplyr::summarize_all(median) %>% 
+    as.data.frame()
   rownames(aibs_med_exprs_broad) = aibs_med_exprs_broad$contam_type
   
-  facs_df$contam_type = paste0(facs_df$contam_type, "_on")
-  aibs_contam_all_sub = calcContamAllTypes(facs_df, markers)
+  facs_df$contam_type = paste0(facs_df$contam_type, 
+    "_on")
+  aibs_contam_all_sub = calcContamAllTypes(facs_df, 
+    markers)
   aibs_contam_all_sub$contam_type = factor(facs_df$contam_type)
   aibs_med_exprs_sub = aibs_contam_all_sub %>% dplyr::group_by(contam_type) %>% 
     dplyr::summarize_all(median) %>% as.data.frame()
@@ -117,24 +133,26 @@ calculatePatchSeqQCMetrics2 <- function(pat_df, facs_df, markers) {
   aibs_med_exprs = rbind(aibs_med_exprs_broad, aibs_med_exprs_sub)
   
   
-  dataset_cell_types = unique(pat_df$major_type) %>% as.character()
+  dataset_cell_types = unique(pat_df$major_type) %>% 
+    as.character()
   marker_sums = lapply(dataset_cell_types, function(cell_type) {
-    curr_marker_type = pat_df[pat_df$major_type == cell_type, 
-      "major_type"][1] %>% as.character
-    # curr_marker_type = paste0(curr_marker_type, '_on')
+    curr_marker_type = pat_df[pat_df$major_type == 
+      cell_type, "major_type"][1] %>% as.character
+    # curr_marker_type = paste0(curr_marker_type,
+    # '_on')
     curr_marker_list = markers[[curr_marker_type]]
     cell_inds = which(pat_df$major_type == curr_marker_type)
     df = pat_df[cell_inds, ]
     rownames(df) = df$sample_id
     marker_expr = sumExpression(df, curr_marker_list)
-    compare_cell_types = setdiff(c("Astro", "Macrophage", "Endo", 
-      "Oligo", "Peri", "SMC", "VLMC", "Glutamatergic", "GABAergic"), 
-      cell_type)
+    compare_cell_types = setdiff(c("Astro", "Macrophage", 
+      "Endo", "Oligo", "Peri", "SMC", "VLMC", 
+      "Glutamatergic", "GABAergic"), cell_type)
     
     mks <- markers[c(curr_marker_type, compare_cell_types)] %>% 
       unlist %>% unique
-    compare_expr_profile = facs_df[facs_df$major_type == cell_type, 
-      mks] %>% log2 %>% colMeans
+    compare_expr_profile = facs_df[facs_df$major_type == 
+      cell_type, mks] %>% log2 %>% colMeans
     contam_values = calcContamAllTypes(df, markers)
     contam_values$contam_type = cell_type
     
@@ -142,33 +160,39 @@ calculatePatchSeqQCMetrics2 <- function(pat_df, facs_df, markers) {
     expected_expr = aibs_med_exprs[contam_values$contam_type[1], 
       compare_cell_types] %>% unlist()  # Expression of all cell types in cluster of interest
     compare_cell_type_exprs = aibs_med_exprs[compare_cell_types, 
-      compare_cell_types] %>% as.matrix %>% diag
-    normalizedContamValues = apply(contam_values[, compare_cell_types], 
-      1, function(x) (x - expected_expr)/(compare_cell_type_exprs - 
-        expected_expr))
+      compare_cell_types] %>% as.matrix %>% 
+      diag
+    normalizedContamValues = apply(contam_values[, 
+      compare_cell_types], 1, function(x) (x - 
+      expected_expr)/(compare_cell_type_exprs - 
+      expected_expr))
     
-    contam_sum = normalizedContamValues %>% repWZero() %>% colSums
+    contam_sum = normalizedContamValues %>% repWZero() %>% 
+      colSums
     
-    marker_sum_norm = normalizeContam(contam_values, aibs_med_exprs, 
-      c(curr_marker_type, compare_cell_types))
+    marker_sum_norm = normalizeContam(contam_values, 
+      aibs_med_exprs, c(curr_marker_type, compare_cell_types))
     marker_sum_norm_vec = contam_values[, curr_marker_type]/aibs_med_exprs[curr_marker_type, 
       curr_marker_type]
     
     
-    quality_score = rbind(compare_expr_profile %>% t() %>% as.data.frame, 
-      df[, mks] %>% log2) %>% t() %>% cor(method = "spearman")
+    quality_score = rbind(compare_expr_profile %>% 
+      t() %>% as.data.frame, df[, mks] %>% log2) %>% 
+      t() %>% cor(method = "spearman")
     quality_score = quality_score[1, -1]
     
-    out_df = data.frame(sample_id = rownames(df), marker_sum = marker_expr, 
-      marker_sum_norm = marker_sum_norm_vec, contam_sum = contam_sum, 
-      quality_score = quality_score)
-    out_df = cbind(out_df, marker_sum_norm %>% t())
+    out_df = data.frame(sample_id = rownames(df), 
+      marker_sum = marker_expr, marker_sum_norm = marker_sum_norm_vec, 
+      contam_sum = contam_sum, quality_score = quality_score)
+    out_df = cbind(out_df, marker_sum_norm %>% 
+      t())
     return(out_df)
   })
-  marker_sums = dplyr::bind_rows(marker_sums) %>% dplyr::select(sample_id, 
-    dplyr::everything())
+  marker_sums = dplyr::bind_rows(marker_sums) %>% 
+    dplyr::select(sample_id, dplyr::everything())
   marker_sums = merge(pat_df %>% dplyr::select(dplyr::one_of("sample_id", 
-    "major_type", "contam_type")), marker_sums, by = "sample_id")
+    "major_type", "contam_type")), marker_sums, 
+    by = "sample_id")
   marker_sum_df = marker_sums
   marker_sum_df = marker_sum_df[order(match(marker_sum_df$sample_id, 
     pat_df$sample_id)), ]
